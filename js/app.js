9b0a2eedfb412f1a1d5735edcb3878442e4c74c7
petercz1 <email.peter.carroll@gmail.com>
2017-11-24 11:56:23 +0800

    Magic is always the best solution -- especially reliable magic.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 // create our angular app and inject ui.bootstrap
-angular.module('app', ['ui.bootstrap'])
+angular.module('app', ['ui.bootstrap']).controller('mainController', function ($scope) {
 
-.controller('mainController', function($scope) {
+    // BUTTONS ======================
 
-  // BUTTONS ======================
+    // define some random object and button values
+    $scope.bigData = {};
 
-  // define some random object and button values
-  $scope.bigData = {};
+    $scope.bigData.breakfast = false;
+    $scope.bigData.lunch = false;
+    $scope.bigData.dinner = false;
+    // pc added this console.log
+    console.log($scope.bigData);
+    // COLLAPSE =====================
+    $scope.isCollapsed = false;
 
-  $scope.bigData.breakfast = false;
-  $scope.bigData.lunch = false;
-  $scope.bigData.dinner = false;
-
-  // COLLAPSE =====================
-  $scope.isCollapsed = false;
-
-});
+  });
